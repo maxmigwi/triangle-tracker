ab8ab6b554ff899f5f1013b78b99d2d01ad82045
maxwel <maxwelmigwi@gmail.com>
2019-06-03 12:09:03 +0300

    something

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ function tracker() {
 
 
     if (triangles[0] + triangles[1] <= triangles[2] || triangles[0] + triangles[2] <= triangles[1] || triangles[1] + triangles[2] <= triangles[0]) {
-      document.getElementById("response").innerHTML = " I'm afraid to tell you this, but you CANNOT form a triangle using those values";
+      
     } else if (triangles[0] === triangles[1] && triangles[1] === triangles[2] && triangles[0] === triangles[2]) {
-
-      document.getElementById("response").innerHTML = " No doubt.That is an Equilateral triangle.";
 
     } else if (triangles[0] === triangles[1] || triangles[0] === triangles[2] || triangles[1] === triangles[2]) {
       document.getElementById("response").innerHTML = " Brilliant! That is an Isosceles triangle.";
-    } else if (triangles[0] + triangles[1] > triangles[2] || triangles[1] + triangles[2] > triangles[0] || triangles[0] + triangles[2] > triangles[1]) {
-      document.getElementById("response").innerHTML = " Wow! That is a Scalene triangle.";
+    } else if (triangles[0] + triangles[1] > triangles[2] || triangles[1] + triangles[2] > triangles[0] || triangles[0] + triangles[2]  document.getElementById("response").
     } else {
-      response.innerHTML = ' Oops! Am sorry that is NOT a triangle!';
+
     }
   }
