25d318b3b8264d6dc500f312548b00f9df3e67b8
maxwel <maxwelmigwi@gmail.com>
2019-06-03 11:00:52 +0300

    add the  changes  to  the  scripts  file  .'

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-function confirmTriangle(){
+
+function tracker() {
+    var x = parseInt(document.getElementById("one").value);
+    var y = parseInt(document.getElementById("two").value);
+    var z = parseInt(document.getElementById("three").value);
+    var triangles = [x, y, z];
 
 
-var a=parseInt(document.getElementById('a').value)
-var b=parseInt(document.getElementById('b').value)
-var c=parseInt(document.getElementById('c').value)
+    if (triangles[0] + triangles[1] <= triangles[2] || triangles[0] + triangles[2] <= triangles[1] || triangles[1] + triangles[2] <= triangles[0]) {
+      document.getElementById("response").innerHTML = " I'm afraid to tell you this, but you CANNOT form a triangle using those values";
+    } else if (triangles[0] === triangles[1] && triangles[1] === triangles[2] && triangles[0] === triangles[2]) {
 
-if(a===b && b===c && c===a){
-  alert("This is an equilateral triangle");
-}else if(a===b && b===c && c!==a){
-  return(This is an Isosceles triangle);
-}
-else {
-  return(scalene);
-}
+      document.getElementById("response").innerHTML = " No doubt.That is an Equilateral triangle.";
 
-
-
-
-
-
-
-}
+    } else if (triangles[0] === triangles[1] || triangles[0] === triangles[2] || triangles[1] === triangles[2]) {
+      document.getElementById("response").innerHTML = " Brilliant! That is an Isosceles triangle.";
+    } else if (triangles[0] + triangles[1] > triangles[2] || triangles[1] + triangles[2] > triangles[0] || triangles[0] + triangles[2] > triangles[1]) {
+      document.getElementById("response").innerHTML = " Wow! That is a Scalene triangle.";
+    } else {
+      response.innerHTML = ' Oops! Am sorry that is NOT a triangle!';
+    }
+  }
